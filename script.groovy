ea43628452bbec92af0a0adaa1781fac07be150b
Muhammed Afsal <60968745+MuhammedAfsal91@users.noreply.github.com>
2023-02-24 17:33:47 +0400

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -4,11 +4,11 @@ def buildJar() {
 } 
 
 def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+                    echo "building the docker image"
+                    withCredentials([usernamePassword(credentialsId:'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+                        sh 'docker build -t muhammedafsal/demo-app:mvn1.1 .'
+                        sh "echo $PASS |  docker login -u $USER --password-stdin"
+                        sh "docker push muhammedafsal/demo-app:mvn1.1"
     }
 } 
 
